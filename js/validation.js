77a7f8867af0a478def88f5c4bffb3e153c980e3
Oksana Tarasova <oksanchik_90@mail.ru>
2025-03-01 10:22:17 +0300

    Исправлено по комментариям

--- js/validation.js
+++ js/validation.js
@@ -1,17 +1,26 @@
+const HASHTAG_QUANTITY = 5;
+const COMMENT_MAX_LENGTH = 140;
+const HASHTAG_REGEXP = /^#[a-zа-яё0-9]{1,19}$/i;
+
+const Errors = {
+  hashtagQuantity: 'Превышено количество хэштегов',
+  hashtagName:'Введён невалидный хэштег',
+  hashtagDuplicate:'Хэштеги повторяются',
+  commentLength: 'Длина комментария больше 140 символов'
+};
+
 const uploadForm = document.querySelector('.img-upload__form');
 const hashtagField = uploadForm.querySelector('.text__hashtags');
 const descriptionField = uploadForm.querySelector('.text__description');
-const HASHTAG_QUANTITY = 5;
-const COMMENT_MAX_LENGTH = 140;
+
 
 const pristine = new Pristine (uploadForm, {
   classTo: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
   errorTextTag: 'div',
   errorTextClass: 'img-upload__field-wrapper--error'
-}, false);
+});
 
-const hashtagRegExp = /^#[a-zа-яё0-9]{1,19}$/i;
 
 const getHashtags = (value) => value.trim() ? value.toLowerCase().trim().split(/\s+/) : [];
 
@@ -22,20 +31,20 @@ const validateHashtagsQuantity = (value) => {
 
 const validateHashtagName = (value) => {
   const hashtags = getHashtags(value);
-  return hashtags.length === 0 || hashtags.every((hashtag) => hashtagRegExp.test(hashtag));
+  return hashtags.length === 0 || hashtags.every((hashtag) => HASHTAG_REGEXP.test(hashtag));
 };
 
-const validateUniqueHashtagName = (value) => {
+const validateHashtagDuplicate = (value) => {
   const hashtags = getHashtags(value);
   return hashtags.length === 0 || new Set(hashtags).size === hashtags.length;
 };
 
 const validateDescription = (value) => value.trim().length <= COMMENT_MAX_LENGTH;
 
-pristine.addValidator(hashtagField, validateHashtagsQuantity, 'Превышено количество хэштегов');
-pristine.addValidator(hashtagField, validateHashtagName, 'Введён невалидный хэштег');
-pristine.addValidator(hashtagField, validateUniqueHashtagName, 'Хэштеги повторяются');
-pristine.addValidator(descriptionField, validateDescription, 'Длина комментария больше 140 символов');
+pristine.addValidator(hashtagField, validateHashtagsQuantity, Errors.hashtagQuantity);
+pristine.addValidator(hashtagField, validateHashtagName, Errors.hashtagName);
+pristine.addValidator(hashtagField, validateHashtagDuplicate, Errors.hashtagDuplicate);
+pristine.addValidator(descriptionField, validateDescription, Errors.commentLength);
 
 uploadForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
