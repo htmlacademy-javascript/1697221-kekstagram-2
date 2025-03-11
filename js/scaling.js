791f0806dff4116fddc176395cdc3bfbf36b540e
Oksana Tarasova <oksanchik_90@mail.ru>
2025-03-11 11:00:10 +0300

    Записала значения в константу

--- js/scaling.js
+++ js/scaling.js
@@ -2,6 +2,10 @@ const DEFAULT_SCALE_VALUE = 100;
 const SCALE_STEP = 25;
 const MAX_SCALE_VALUE = 100;
 const MIN_SCALE_VALUE = 25;
+const SCALE_DIRECTION = {
+  INCREASE: 'increase',
+  DECREASE: 'decrease',
+};
 
 const controlScaleValue = document.querySelector('.scale__control--value');
 const imagePreview = document.querySelector('.img-upload__preview');
@@ -10,9 +14,9 @@ let currentValue = DEFAULT_SCALE_VALUE;
 
 
 const changeScale = (direction) => {
-  if (direction === 'increase' && currentValue < MAX_SCALE_VALUE) {
+  if (direction === SCALE_DIRECTION.INCREASE && currentValue < MAX_SCALE_VALUE) {
     currentValue += SCALE_STEP;
-  } else if (direction === 'decrease' && currentValue > MIN_SCALE_VALUE) {
+  } else if (direction === SCALE_DIRECTION.DECREASE && currentValue > MIN_SCALE_VALUE) {
     currentValue -= SCALE_STEP;
   }
 };
